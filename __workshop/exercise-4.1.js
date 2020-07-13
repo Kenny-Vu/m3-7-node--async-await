6f0c790ad16e47ca87f043fe22765bfa19a82de2
Kenny Vu <kennyvu@live.ca>
2020-07-13 15:23:13 -0400

    question 4.1 finished!

--- __workshop/exercise-4.1.js
+++ __workshop/exercise-4.1.js
@@ -1,6 +1,26 @@
-const request = require('request-promise');
+const request = require("request-promise");
+const requestPromise = require("request-promise");
 
 // getDadJoke
+let jokeReq = {
+  uri: "https://icanhazdadjoke.com/",
+  qs: "https://icanhazdadjoke.com/api#fetch-a-dad-joke",
+  headers: {
+    "User-Agent": "Request-Promise",
+    Accept: "application/json",
+  },
+  json: true,
+};
+
+getJoke = async () => {
+  try {
+    const jokeRes = await requestPromise(jokeReq);
+    const joke = await jokeRes.joke;
+    return joke;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 // 4.1
-// getJoke().then((data) => console.log(data));
+getJoke().then((data) => console.log(data));
